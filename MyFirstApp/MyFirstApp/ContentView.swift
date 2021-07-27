0b0e04d57b9c89d9520351f9f125a083ca9493c2
marie flores <marieflores.contact@gmail.com>
2021-07-27 17:12:39 +0200

    [feature]create surfspot class

--- MyFirstApp/MyFirstApp/ContentView.swift
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var spots = ["SkeletonBay", "Mentawai", "Hawai"]
+    let surfSpot1 = SurfSpot(id: 1, surfBreak:"SkeletonBay", photos:"Hawai", address:"somewhere")
+    var spots = [surfSpot1, "Mentawai", "Hawai"]
     
     var body: some View {
         List {
@@ -18,9 +19,10 @@ struct ContentView: View {
                 .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
             ForEach(spots, id: \.self){ spot in
                 HStack {
-                    Text(spot)
+                    Text(surfSpot1.surfBreak)
+                    Text(surfSpot1.address)
                     Spacer()
-                    Image(spot)
+                    Image(surfSpot1.photos)
                         .resizable()
                         .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
                         .frame(width: 100, height: 80)
---
+++ MyFirstApp/MyFirstApp/SurfSpot.swift
@@ -0,0 +1,29 @@
+//
+//  SurfSpot.swift
+//  MyFirstApp
+//
+//  Created by marie flores on 27/07/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct SurfSpot : Hashable, Codable {
+    var id: Int
+    var surfBreak: String
+    var photos: String
+    var address: String
+    
+    init(id: Int, surfBreak: String, photos: String, address: String) {
+        self.id = id
+        self.surfBreak = surfBreak
+        self.photos = photos
+        self.address = address
+    }
+    
+//    private var imageName: String
+//      var image: Image {
+//          Image(imageName)
+//      }
+    
+}
